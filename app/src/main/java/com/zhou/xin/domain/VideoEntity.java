ed5128d335d481086a919c88a76437480da4b255
zhengjinzhou <1457594845@qq.com>
2017-12-20 12:12:12 +0800

    新增了视频的发送功能，但是发现点击已经发送的视频或者照片的时候，程序崩溃了

---
+++ app/src/main/java/com/zhou/xin/domain/VideoEntity.java
@@ -0,0 +1,13 @@
+package com.zhou.xin.domain;
+
+/**
+ * Created by zhou on 2017/12/20.
+ */
+
+public class VideoEntity {
+    public int ID;
+    public String title;
+    public String filePath;
+    public int size;
+    public int duration;
+}
