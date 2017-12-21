e805637d28252a08896da302bfa3e1f478bb561f
zhengjinzhou <1457594845@qq.com>
2017-12-21 09:54:28 +0800

    完善群里所需要的部分功能

--- app/src/main/java/com/zhou/xin/ui/activity/NewGroupActivity.java
+++ app/src/main/java/com/zhou/xin/ui/activity/NewGroupActivity.java
@@ -1,15 +1,123 @@
 package com.zhou.xin.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMGroupManager;
+import com.hyphenate.chat.EMGroupOptions;
+import com.hyphenate.easeui.widget.EaseAlertDialog;
+import com.hyphenate.exceptions.HyphenateException;
 import com.zhou.xin.R;
+import com.zhou.xin.base.BaseActivity;
 
-public class NewGroupActivity extends AppCompatActivity {
+import butterknife.BindView;
+
+public class NewGroupActivity extends BaseActivity {
+
+    @BindView(R.id.edit_group_name) EditText groupNameEditText;
+    @BindView(R.id.edit_group_introduction) EditText introductionEditText;
+    @BindView(R.id.cb_public) CheckBox publibCheckBox;
+    @BindView(R.id.cb_member_inviter) CheckBox memberCheckbox;
+    @BindView(R.id.second_desc) TextView secondTextView ;
+    private ProgressDialog progressDialog;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_group);
+    public int getLayout() {
+        return R.layout.activity_new_group;
     }
+
+    @Override
+    public void initV() {
+        publibCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    secondTextView.setText(R.string.join_need_owner_approval);
+                }else{
+                    secondTextView.setText(R.string.Open_group_members_invited);
+                }
+            }
+        });
+    }
+
+    /**
+     * @param v
+     */
+    public void save(View v) {
+        String name = groupNameEditText.getText().toString();
+        if (TextUtils.isEmpty(name)) {
+            new EaseAlertDialog(this, R.string.Group_name_cannot_be_empty).show();
+        } else {
+            // select from contact list
+            startActivityForResult(new Intent(this, GroupPickContactsActivity.class).putExtra("groupName", name), 0);
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        String st1 = getResources().getString(R.string.Is_to_create_a_group_chat);
+        final String st2 = getResources().getString(R.string.Failed_to_create_groups);
+        if (resultCode == RESULT_OK) {
+            //new group
+            progressDialog = new ProgressDialog(this);
+            progressDialog.setMessage(st1);
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    final String groupName = groupNameEditText.getText().toString().trim();
+                    String desc = introductionEditText.getText().toString();
+                    String[] members = data.getStringArrayExtra("newmembers");
+                    try {
+                        EMGroupOptions option = new EMGroupOptions();
+                        option.maxUsers = 200;
+                        option.inviteNeedConfirm = true;
+
+                        String reason = NewGroupActivity.this.getString(R.string.invite_join_group);
+                        reason  = EMClient.getInstance().getCurrentUser() + reason + groupName;
+
+                        if(publibCheckBox.isChecked()){
+                            option.style = memberCheckbox.isChecked() ? EMGroupManager.EMGroupStyle.EMGroupStylePublicJoinNeedApproval : EMGroupManager.EMGroupStyle.EMGroupStylePublicOpenJoin;
+                        }else{
+                            option.style = memberCheckbox.isChecked()? EMGroupManager.EMGroupStyle.EMGroupStylePrivateMemberCanInvite: EMGroupManager.EMGroupStyle.EMGroupStylePrivateOnlyOwnerInvite;
+                        }
+                        EMClient.getInstance().groupManager().createGroup(groupName, desc, members, reason, option);
+                        runOnUiThread(new Runnable() {
+                            public void run() {
+                                progressDialog.dismiss();
+                                setResult(RESULT_OK);
+                                finish();
+                            }
+                        });
+                    } catch (final HyphenateException e) {
+                        runOnUiThread(new Runnable() {
+                            public void run() {
+                                progressDialog.dismiss();
+                                Toast.makeText(NewGroupActivity.this, st2 + e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                            }
+                        });
+                    }
+
+                }
+            }).start();
+        }
+    }
+
+    public void back(View view) {
+        finish();
+    }
+
 }
